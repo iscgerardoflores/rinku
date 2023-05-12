9cfda153a10c0da4a41a0a7eca54b1b9bb4e6e05
iscgerardoflores <isc.gerardoflores@gmail.com>
2023-05-11 22:39:30 -0600

    Add files via upload

--- rinku.sql
+++ rinku.sql
@@ -36,7 +36,7 @@ CREATE TABLE `empleados` (
 /*Data for the table `empleados` */
 
 insert  into `empleados`(`id`,`numempleado`,`nombre`,`apellidoPaterno`,`apellidoMaterno`,`id_rol`,`fecha_creacion`,`fecha_ultimo_cambio`,`deleted`) values 
-(1,100,'Gerardo','Flores','De La Cruz',1,'2023-05-10 22:04:46','2023-05-10 22:04:51',NULL),
+(1,100,'Gerardo','Flores','De La Cruz',2,'2023-05-10 22:04:46','2023-05-12 03:52:18',NULL),
 (2,200,'Alejandro','Flores','De la Cruz',2,'2023-05-11 04:48:35','2023-05-11 04:48:35',NULL),
 (3,300,'Alejandro','Flores','De la Cruz',3,'2023-05-11 04:50:10','2023-05-11 04:50:10',NULL),
 (4,400,'Manuel','Flores','De la Cruz',1,'2023-05-11 04:50:59','2023-05-11 04:50:59',NULL),
@@ -45,9 +45,9 @@ insert  into `empleados`(`id`,`numempleado`,`nombre`,`apellidoPaterno`,`apellido
 (7,700,'Adrian','Flores','De la Cruz',1,'2023-05-11 04:56:54','2023-05-11 04:56:54',NULL),
 (8,800,'Enrique','Flores','De la Cruz',2,'2023-05-11 05:00:56','2023-05-11 05:00:56',NULL),
 (9,900,'Javier','Flores','De la Cruz',2,'2023-05-11 05:01:14','2023-05-11 05:01:14',NULL),
-(10,10000,'Heriberto','Flores','De la Cruz',2,'2023-05-11 05:25:02','2023-05-11 05:25:02',NULL),
+(10,10000,'Heriberto','Osorio','Flores',3,'2023-05-11 05:25:02','2023-05-12 02:03:57',NULL),
 (11,1100,'Mario','Flores','De la Cruz',1,'2023-05-11 05:28:09','2023-05-11 05:28:09',NULL),
-(12,1300,'HUmberto','Flores','De la Cruz',1,'2023-05-11 05:57:09','2023-05-11 05:57:09',NULL);
+(12,1300,'Humberto','Flores','De la Cruz',1,'2023-05-11 05:57:09','2023-05-12 02:04:18',NULL);
 
 /*Table structure for table `movimientos` */
 
@@ -64,9 +64,22 @@ CREATE TABLE `movimientos` (
   `fecha_ultimo_cambio` datetime DEFAULT NULL,
   `deleted` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `movimientos` */
+
+insert  into `movimientos`(`id`,`numempleado`,`nombre`,`id_rol`,`mes`,`num_entregas`,`fecha_creacion`,`fecha_ultimo_cambio`,`deleted`) values 
+(1,100,'Gerardo',2,5,10,'2023-05-12 04:09:13','2023-05-12 04:09:13',NULL),
+(2,100,'Gerardo',2,5,10,'2023-05-12 04:10:46','2023-05-12 04:10:46',NULL),
+(3,200,'Alejandro',2,5,10,'2023-05-12 04:11:52','2023-05-12 04:11:52',NULL),
+(4,700,'Adrian',1,5,10,'2023-05-12 04:12:35','2023-05-12 04:12:35',NULL),
+(5,900,'Javier',2,1,5,'2023-05-12 04:21:07','2023-05-12 04:21:07',NULL),
+(6,500,'Arturo',2,7,10,'2023-05-12 04:21:18','2023-05-12 04:21:18',NULL),
+(7,200,'Alejandro',2,4,10,'2023-05-12 04:21:49','2023-05-12 04:21:49',NULL),
+(8,800,'Enrique',2,11,5,'2023-05-12 04:22:04','2023-05-12 04:22:04',NULL),
+(9,1100,'Mario',1,5,10,'2023-05-12 04:22:13','2023-05-12 04:22:13',NULL),
+(10,300,'Alejandro',3,11,5,'2023-05-12 04:22:23','2023-05-12 04:22:23',NULL),
+(11,600,'Antonio',3,11,8,'2023-05-12 04:22:41','2023-05-12 04:22:41',NULL);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
